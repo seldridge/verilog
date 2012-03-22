f58caf5d8e2d7c7dc9edbc33ad5e0ddadd3db08c
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2012-03-22 12:26:32 -0400

    Updated debounce circuit to 3 always block format currently in use.

--- button_debounce.v
+++ button_debounce.v
@@ -1,12 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Schuyler Eldridge (schuyler.eldridge@gmail.com)
 // button_debounce.v
-// Created: 10/10/2009 
+// Created: 10/10/2009
+// Modified: 3/20/2012
 //
 // Counter based debounce circuit originally written for EC551 (back
-// in the day).
+// in the day) and then modified (i.e. chagned entirely) into 3 always
+// block format. This debouncer generates a signal that goes high for
+// 1 clock cycle after the clock sees an asserted value on the button
+// line. This action is then disabled until the counter hits a
+// specified count value that is determined by the clock frequency and
+// desired debounce frequency. An alternative implementation would not
+// use a counter, but would use the shift register approach, looking
+// for repeated matches (say 5) on the button line.
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright (C) 2009 Schuyler Eldridge, Boston University
+// Copyright (C) 2012 Schuyler Eldridge, Boston University
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -20,50 +28,57 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ////////////////////////////////////////////////////////////////////////////////
-`timescale 1ns / 1ps
 module button_debounce(
                        input  clk,
                        input  button,
-                       output debounce
+                       output reg debounce
                        );
   
   parameter
-    count_value = 25000000;
-  // values of count_value correspond to max output frequency of debounce:
-  // count_value (clock cycles) | debounce (Hz)|
-  // ------------------------------------------|
-  // 200,000,000                | 0.25         |
-  // 100,000,000                | 0.50         |
-  //  50,000,000                | 1.00         |
-  //  25,000,000                | 2.00         |
-  //  12,500,000                | 4.00         |
-  //   6,250,000                | 8.00         |
-  // ------------------------------------------|
-  // *NOTE* This assumes a 50MHz clock
-  
-  reg                         count_en;
-  reg                         debounce;
+    CLK_FREQUENCY  = 66000000,
+    DEBOUNCE_HZ    = 2;
+  // These parameters are specified such that you can choose any power
+  // of 2 frequency for a debouncer between 1 Hz and
+  // CLK_FREQUENCY. Note, that this will throw errors if you choose a
+  // non power of 2 frequency (i.e. count_value evaluates to some
+  // number / 3 which isn't interpreted as a logical right shift). I'm
+  // assuming this will not work for DEBOUNCE_HZ values less than 1,
+  // however, I'm uncertain of the value of a debouncer for fractional
+  // hertz button presses.
+  localparam
+    COUNT_VALUE  = CLK_FREQUENCY / DEBOUNCE_HZ,
+    WAIT         = 0,
+    FIRE         = 1,
+    COUNT        = 2;
+
+  reg [1:0]                   state, next_state;
   reg [25:0]                  count;
-  
+
+  always @ (posedge clk)
+    state <= next_state;
+
   always @ (posedge clk) begin
-    // exit counting state
-    if (count >= count_value) begin
-      count_en <= 0;
-      count <= 0;
-      debounce <= 0;
-      // counting state
-    end
-    else if (count_en == 1) begin
-      count_en <= 1;
-      count <= count + 1;
-      debounce <= 0;
-    end
-    // output generation state
-    else if (button == 1) begin
-      count_en <= 1;
-      count <= 0;
-      debounce <= 1;
-    end
+    debounce <= 0;
+    count    <= 0;
+    case (state)
+      WAIT: begin
+      end
+      FIRE: begin
+        debounce <= 1;
+      end
+      COUNT: begin
+        count <= count + 1;
+      end
+    endcase // case (state)
   end
 
-endmodule
+  always @ * begin
+    case (state)
+      WAIT:    next_state = (button)                  ? FIRE : state;
+      FIRE:    next_state = COUNT;
+      COUNT:   next_state = (count > COUNT_VALUE - 1) ? WAIT : state;
+      default: next_state = WAIT;
+    endcase // case (state)
+  end
+
+endmodule // button_debounce
