a64aa07f484b5c6008485b080d659141716df487
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2012-07-16 22:27:20 -0400

    Added token timescale directive to sign_extender.v

--- src/sign_extender.v
+++ src/sign_extender.v
@@ -21,6 +21,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/1ps
 module sign_extender
   #(
     parameter
